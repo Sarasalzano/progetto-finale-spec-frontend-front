11dcf40a45651d6ffd2f4192553e929b4c919b6e
Sara Salzano <sarasalzano95@gmail.com>
2026-01-20 15:47:09 +0100

    imposta struttura base carddetailpage

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,16 +1,15 @@
-import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Card({ cities }) {
-    console.log(cities)
   return (
     <div className="cards-container">
       {cities.map((city) => (
-        <div className="card" key={city.id}>
+        <Link to={`/carddetail/${city.id}`} key={city.id} className="card">
           <div className="text-container">
             <h3>{city.title}</h3>
             <span>{city.category}</span>
           </div>
-        </div>
+        </Link>
       ))}
     </div>
   );
--- src/pages/CardDetailPage.jsx
+++ src/pages/CardDetailPage.jsx
@@ -1,3 +1,38 @@
-export default function CardDetailPage(){
-    return <div>CardDetailPage</div>
+import { useEffect, useState } from "react";
+//UseParams per leggere l'id dall' URL
+import { useParams } from "react-router-dom";
+
+export default function CardDetailPage() {
+  const [cityDetail, setCityDetail] = useState(null);
+
+  //Id dall' URL per fare fetch del dettaglio della città
+  const { id } = useParams();
+
+  //Useeffect per fare fetch dei dettagli della città quando cambia l'id
+  useEffect(() => {
+    const fetchSingleCity = async () => {
+      try {
+        const response = await fetch(`http://localhost:3001/cities/${id}`);
+        const data = await response.json();
+        setCityDetail(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchSingleCity();
+  }, [id]);
+
+  return (
+    <div className="detailpage-container">
+      {cityDetail.map((city) => (
+        <header key={city.id}>
+          <span>{city.category}</span>
+          <h2>{city.title}</h2>
+          <span>{city.country}</span>
+        </header>
+        
+      ))}
+    </div>
+  );
 }

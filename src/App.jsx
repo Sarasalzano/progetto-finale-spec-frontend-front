badeb5007271a72fb60d804a1f8a49571c7cc1ec
Sara Salzano <sarasalzano95@gmail.com>
2026-01-20 10:39:50 +0100

    inserisci rotte

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import CardDetailPage from "./pages/CardDetailPage";
+import FavouritesPage from "./pages/FavouritesPage";
 
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+          <Route path="/carddetail" element={<CardDetailPage />} />
+          <Route path="/favourites" element={<FavouritesPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;

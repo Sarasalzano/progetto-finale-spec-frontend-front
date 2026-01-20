45726ba3e332541ef66b3427a7b8e522db5c5b3b
Sara Salzano <sarasalzano95@gmail.com>
2026-01-20 14:57:00 +0100

    aggiungi struttura card e stile base

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 
 export default function Card({ cities }) {
+    console.log(cities)
   return (
     <div className="cards-container">
       {cities.map((city) => (
         <div className="card" key={city.id}>
-          <div className="img-wrapper">
-            <img src={city.image} alt={city.title} />
-          </div>
           <div className="text-container">
             <h3>{city.title}</h3>
             <span>{city.category}</span>
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,11 +1,15 @@
 import Header from "../components/Header";
 import Hero from "../components/Hero";
 import CityControls from "../components/CityControls";
+import CardList from "../components/CardList";
 
-export default function Homepage(){
-    return <>
-    <Header />
-    <Hero />
-    <CityControls />
+export default function Homepage() {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <CityControls />
+      <CardList />
     </>
+  );
 }

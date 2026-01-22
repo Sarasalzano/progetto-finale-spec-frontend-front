0e45c32bcd61d8bbafd62ce2ab1d683a6629cda2
Sara Salzano <sarasalzano95@gmail.com>
2026-01-22 15:09:08 +0100

    inserisci props aggiornata delle città filtrate sortedcities

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,13 @@
 import { Link } from "react-router-dom";
 
-export default function Card({ cities }) {
+export default function Card({ sortedCities }) {
   return (
     <div className="cards-container">
-      {cities.map((city) => (
-        <Link to={`/cities/${city.id}`} key={city.id} className="card">
+      {sortedCities?.map((city) => (
+        <Link to={`/cities/${city?.id}`} key={city?.id} className="card">
           <div className="text-container">
-            <h3>{city.title}</h3>
-            <span>{city.category}</span>
+            <h3>{city?.title}</h3>
+            <span>{city?.category}</span>
           </div>
         </Link>
       ))}
--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -1,25 +1,9 @@
 import Card from "./Card";
-import { useEffect, useState } from "react";
 
-export default function CardList() {
-  const [cities, setCities] = useState([]);
-
-  useEffect(() => {
-    const fetchCards = async () => {
-      try {
-        const response = await fetch("http://localhost:3001/cities");
-        const data = await response.json();
-        setCities(data);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-    fetchCards();
-  }, []);
-
+export default function CardList({ sortedCities }) {
   return (
     <div className="card-row">
-      <Card cities={cities} />
+      <Card sortedCities={sortedCities } />
     </div>
   );
 }
